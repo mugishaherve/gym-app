b133120b2cf6ffeca7125cd4027f587123737e51
herve <mugishaherve1000@gmail.com>
2023-10-03 13:33:45 +0200

    add

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -11,85 +11,94 @@ import HomePageGraphic from '@/assets/HomePageGraphic.png'
 import {motion} from "framer-motion"
 
 type Props = {
-    setSelectedPage: (value: SelectedPage) => void
-}
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Home = ({setSelectedPage }: Props) => {
-    
-    const isAboveMediumScreens= useMediaQuery("(min-width: 1060px)")
-    return <section
-        id='home'
-        className='gap-16 bg-gray-20 py-10 md: h-full nd: pb-0'>
-        {/* image and main header */}
-        <motion.div
-            className='md: flex mx-auto w-5/6 items-center justify-center'
-            onViewportEnter={()=> setSelectedPage(SelectedPage.Home )}
-        > 
-            {/* main header */}
-            <div className='z-10 mt-32 md:basis-3/5'>
-                   {/* headings */}
-                <motion.div className='md:-mt-20'
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, amount: 0.5 }}
-                    transition={{ duration: 2 }}
-                    variants={{
-                        hidden: { opacity: 0, x: -50 },
-                        visible: {opacity: 1, x: 0}
-                    }}>
-                    <div className='relative'>
-                        <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
-                            <img src={HomePageText} alt="home-page-text" />
-                        </div>
-                    </div>
+const Home = ({ setSelectedPage }: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
 
-                    <p className='mt-8 text-sm'>
-                        Unrivaled Gym. Unparalleled Training Fitness Classes.
-                        World Class Studios to get the Body Shapes That you Dream of.. Get Your Dream
-                        Body Now.
-                    </p>
-                </motion.div>
-                {/* actions */}
-                <motion.div className='mt-8 flex items-center gap-8
-                '  initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, amount: 0.5 }}
-                    transition={{ delay: 0.2, duration: 2 }}
-                    variants={{
-                        hidden: { opacity: 0, x: -50 },
-                        visible: {opacity: 1, x: 0}
-                    }}>
-                    <ActionButton setSelectedPage={setSelectedPage}>
-                        Join Now 
-                    </ActionButton>
-                    <AnchorLink className='text-sm font-bold text-primary-500 underline hover:text-secondary-500'
-                        onClick={() => setSelectedPage(SelectedPage.ContactUs)}
-                    href={`#${SelectedPage.ContactUs}`}>
-                        <p>Learn more</p>
-                   </AnchorLink>
-                </motion.div>
+  return (
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      {/* IMAGE AND MAIN HEADER */}
+      <motion.div
+        className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6"
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      >
+        {/* MAIN HEADER */}
+        <div className="z-10 mt-32 md:basis-3/5">
+          {/* HEADINGS */}
+          <motion.div
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
+                <img alt="home-page-text" src={HomePageText} />
+              </div>
             </div>
-            {/* image */}
-            <div className='flex basis-3/5 justify-center md:z-10
-            md:ml-40 md:mt-16 md:justify-items-end'>
-                <img src={HomePageGraphic} alt="home-page" />
+
+            <p className="mt-8 text-sm">
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+              Studios to get the Body Shapes That you Dream of.. Get Your Dream
+              Body Now.
+            </p>
+          </motion.div>
+
+          {/* ACTIONS */}
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </motion.div>
+        </div>
+
+        {/* IMAGE */}
+        <div
+          className="flex basis-3/5 justify-center md:z-10
+              md:ml-40 md:mt-16 md:justify-items-end"
+        >
+          <img alt="home-pageGraphic" src={HomePageGraphic} />
+        </div>
+      </motion.div>
+
+      {/* SPONSORS */}
+      {isAboveMediumScreens && (
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img alt="redbull-sponsor" src={sponsorRedbull} />
+              <img alt="forbes-sponsor" src={sponsorForbes} />
+              <img alt="fortune-sponsor" src={sponsorFortune} />
             </div>
-        </motion.div>
-        {/* sponsors */}
-        {isAboveMediumScreens && (
-            <div className='h-[150px] w-full bg-primary-100 py-10'>
-                <div className='mx-auto w-5/6'>
-                    <div className='flex w-3/5 items-center justify-between gap-8'>
-                        <img src={sponsorRedbull} alt="redbull-sponsor" />
-                        <img src={sponsorForbes} alt="forbes-sponsor" />
-                         <img src={sponsorFortune} alt="fortune-sponsor" />
-                    </div>
-                </div>
-            </div>
-)}
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
 
-
-  </section>
-}
-
-export default Home
+export default Home;
--- src/scenes/ourClasses/Class.tsx
+++ src/scenes/ourClasses/Class.tsx
@@ -1,24 +1,24 @@
-
-
 type Props = {
-    name: string;
-    description?: string;
-    image: string;
-}
+  name: string;
+  description?: string;
+  image: string;
+};
 
-const classComponent = ({ name, description, image }: Props) => {
-    
-    const overlayStyles = `p-5 absolute z-30 flex h-[380px] w-[450px] flex-col items-center justify-center
-     whitespace-normal bg-primary-500 text-center text-white opacity-0 transition duration-500 hover:opacity-90`
-    return <li className="relative mx-5 in-line-block h-[380px] w-[450px]">
-        <div className={overlayStyles}>
-            <p>{name}</p>
-            <p>{description}</p>
-            <img src={image} alt={`${image}`} />
+const Class = ({ name, description, image }: Props) => {
+  const overlayStyles = `p-5 absolute z-30 flex
+  h-[380px] w-[450px] flex-col items-center justify-center
+  whitespace-normal bg-primary-500 text-center text-white
+  opacity-0 transition duration-500 hover:opacity-90`;
+
+  return (
+    <li className="relative mx-5 inline-block h-[380px] w-[450px]">
+      <div className={overlayStyles}>
+        <p className="text-2xl">{name}</p>
+        <p className="mt-5">{description}</p>
       </div>
-  </li>
-    
-  
-}
+      <img alt={`${image}`} src={image} />
+    </li>
+  );
+};
 
-export default classComponent
+export default Class;
